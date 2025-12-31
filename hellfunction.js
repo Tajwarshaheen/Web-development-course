2433daac4119604d641eb61bced7bff6f67f308a
Tajwarshaheen <wazirsaqlain1011@gmail.com>
2025-12-31 03:46:56 -0800

    woaloa;ld

--- hellfunction.js
+++ hellfunction.js
@@ -18,3 +18,31 @@ function suther(){
     console.log("suther function is running");
 }
 other();
+
+
+let promise = new Promise((resolve, reject) => {
+    let a = 9;
+    if (a%2==0) {
+        resolve()
+                console.log("resolve");
+        
+    } else {
+        reject();
+        console.log("reject");
+        
+
+    }
+})
+
+// let submit = new Promise((resolve, reject) => {
+//     let   submit = 23;
+//     if (submit ==0){
+//         console.log("your data is resolve");
+//         resolve();
+//     }
+//     else{
+//         console.log("your data is reject");
+//         reject();
+        
+//     }
+// })
